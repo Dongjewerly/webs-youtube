267f2a18966997421eabe1d07294eaf52da8f540
Dongseok <117716335+Dongjewerly@users.noreply.github.com>
2024-05-30 17:52:47 +0900

    헤더영역데이터완료

---
+++ src/components/header/Sns.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { snsLink } from '../../data/header'
+
+const Sns = () => {
+  return (
+    <div className='header__sns'>
+        <ul>
+            {snsLink.map((sns,key)=>(
+                <li key={key}>
+                    <a href={sns.url}
+                    rel='noopener noreferrer' 
+                    aria-label={sns.title}
+                    target='_black'
+                    >
+                        <span>{sns.icon}</span>
+                    </a>
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
+
+export default Sns
