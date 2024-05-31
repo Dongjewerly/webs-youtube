38164722736b948eddf32d9251ea922149f1a21d
Dongseok <117716335+Dongjewerly@users.noreply.github.com>
2024-05-31 13:22:03 +0900

    컴포넌트비동기작업완료

--- src/App.js
+++ src/App.js
@@ -1,48 +1,42 @@
-import React from 'react'
+import React, {Suspense,lazy} from 'react'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Main from './components/section/Main'
 
-import Channel from './pages/Channel'
-import Developer from './pages/Developer'
-import Gsap from './pages/Gsap'
-import Home from './pages/Home'
-import Not from './pages/Not'
-import Port from './pages/Port'
-import Search from './pages/Search'
-import Today from './pages/Today'
-import Video from './pages/Video'
-import Webd from './pages/Webd'
-import Website from './pages/Website'
-import Youtube from './pages/Youtube'
-
-import Header from "./components/section/Header";
-import Main from "./components/section/Main";
-import Footer from "./components/section/Footer";
+const Home = lazy(()=>import('./pages/Home'));
+const Channel = lazy(()=>import('./pages/Channel'));
+const Developer = lazy(()=>import('./pages/Developer'));
+const Gsap = lazy(()=>import('./pages/Gsap'));
+const Not = lazy(()=>import('./pages/Not'));
+const Port = lazy(()=>import('./pages/Port'));
+const Search = lazy(()=>import('./pages/Search'));
+const Today = lazy(()=>import('./pages/Today'));
+const Video = lazy(()=>import('./pages/Video'));
+const Webd = lazy(()=>import('./pages/Webd'));
+const Website = lazy(()=>import('./pages/Website'));
+const Youtube = lazy(()=>import('./pages/Youtube'));
 
 
 const App = () => {
   return(
     <BrowserRouter>
-      <Header/>
-      <Main>
+      <Suspense fallback={<Main/>}>
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/today' element={<Today/>}/>
-          <Route path='/developer' element={<Developer/>}/>
-          <Route path='/webd' element={<Webd/>}/>
-          <Route path='/website' element={<Website/>}/>
-          <Route path='/gsap' element={<Gsap/>}/>
-          <Route path='/port' element={<Port/>}/>
-          <Route path='/youtube' element={<Youtube/>}/>
-          <Route path='/channel/:channelId' element={<Channel/>}/>
-          <Route path='/video/:videoId' element={<Video/>}/>
-          <Route path='/search/:searchId' element={<Search/>}/>
-          <Route path='*' element={<Not/>}/>
-        </Routes>
-      </Main>
-      <Footer/>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/today' element={<Today/>}/>
+            <Route path='/developer' element={<Developer/>}/>
+            <Route path='/webd' element={<Webd/>}/>
+            <Route path='/website' element={<Website/>}/>
+            <Route path='/gsap' element={<Gsap/>}/>
+            <Route path='/port' element={<Port/>}/>
+            <Route path='/youtube' element={<Youtube/>}/>
+            <Route path='/channel/:channelId' element={<Channel/>}/>
+            <Route path='/video/:videoId' element={<Video/>}/>
+            <Route path='/search/:searchId' element={<Search/>}/>
+            <Route path='*' element={<Not/>}/>
+          </Routes>
+      </Suspense>
     </BrowserRouter>
-
-  )
+  );
 }
 
 export default App
