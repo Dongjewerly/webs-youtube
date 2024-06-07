45ed08cc0c64514b39ad612d6d2f4321deb58f1b
Dongseok <117716335+Dongjewerly@users.noreply.github.com>
2024-06-07 17:27:20 +0900

    추천개발페이지및콘텐츠

--- src/components/contents/Developer.jsx
+++ src/components/contents/Developer.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 
+import { developerText } from '../../data/developer'
+import {Link} from 'react-router-dom'
+
 const Developer = () => {
   return (
-    <div>
-      developer
-    </div>
+    <section id='developer'>
+      <h2>😪 추천 개발자를 소개합니다.</h2>
+      <div className='developer_inner overflow'>
+        {developerText.map((developer,key)=>(
+          <div className='developer' key={key}>
+            <div className='developer_img play_icon'>
+              <Link to={`/channel/${developer.channelId}`}>
+                <img src={developer.img} alt={developer.name}/>
+              </Link>
+            </div>
+            <div className='developer_info'>
+              <Link to={`/channel/${developer.channelId}`}>
+                {developer.name}
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
--- src/pages/Developer.jsx
+++ src/pages/Developer.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Main from '../components/section/Main'
 
+import { developerText } from '../data/developer'
+import { Link } from 'react-router-dom'
+
 
 const Developer = () => {
     return (
@@ -8,7 +11,25 @@ const Developer = () => {
         title="추천 개발자"
         description="오늘의 추천 개발자입니다."
         >
-            Developer 
+            <section id='developerPage'>
+                <h2>😪 추천 개발자를 소개합니다.</h2>
+                <div className='developer_inner'>
+                    {developerText.map((developer,key)=>(
+                    <div className='developer' key={key}>
+                        <div className='developer_img play_icon'>
+                        <Link to={`/channel/${developer.channelId}`}>
+                            <img src={developer.img} alt={developer.name}/>
+                        </Link>
+                        </div>
+                        <div className='developer_info'>
+                        <Link to={`/channel/${developer.channelId}`}>
+                            {developer.name}
+                        </Link>
+                        </div>
+                    </div>
+                    ))}
+                </div>
+            </section>
         </Main>
     )
 }
